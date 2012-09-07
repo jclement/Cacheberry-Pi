15fd98453fd192f7561862827d12b856247511c9
Jeff Clement <jeffrey.clement@gmail.com>
2012-09-07 13:51:21 +0000

    Fixed bug where GPS data would backup.  New dist calcs

--- GeocacheFinder.py
+++ GeocacheFinder.py
@@ -35,7 +35,7 @@ class CacheDatabase:
     self.__closeRadius = closeRadius
     self.__bearingError = bearingError
   
-  def findNearest(self, lat, lon, bearing):
+  def findNearest(self, lat, lon, bearing, speed):
     cur = self.__conn.cursor()
 
     # SQL to define a circle around the home point with a given radius
@@ -47,7 +47,7 @@ class CacheDatabase:
     dist2 = 'geodesiclength(geomfromtext("linestring(" || x(location) || " " || y(location) || ", %f %f)", 4326))' % (lat, lon)
 
     # Query for caches within circle and order by distance
-    rs = cur.execute('select code, description, x(location), y(location), %(dist)s, %(dist2)s from gc where MbrWithin(location, %(searchCircle)s) order by %(dist)s' % {'dist':dist, 'searchCircle':circle, 'dist2':dist2})
+    rs = cur.execute('select code, description, x(location), y(location), %(dist)s from gc where MbrWithin(location, %(searchCircle)s) order by %(dist)s' % {'dist':dist2, 'searchCircle':circle})
 
     data = []
     for row in rs:
@@ -55,7 +55,10 @@ class CacheDatabase:
       data.append({
         'code': row[0],
         'description': row[1],
-        'distance': int(row[4]),
+        'distance_db': int(row[4]),
+        # I more reliable (I think) distance calcs from this function
+        # rather than the database
+        'distance': 1000.8 * gislib.getDistance(coord, (lat, lon)),
         'human_bearing': humanizeBearing(calculateBearing(coord, (lat, lon))), 
         'bearing': calculateBearing(coord, (lat, lon)),
         'lat': row[2],
@@ -64,9 +67,23 @@ class CacheDatabase:
 
     if len(data) == 0: return None
 
-    if data[0]['distance'] < self.__closeRadius:
-      return data[0] 
+    # sort by distance again since we aren't using the database distance
+    # this should be removed if I switch back to using the Spatialite dist.
+    data.sort(key=lambda x: x['distance'])
 
+
+    # If first cache is within closeRadius return that 
+    if data[0]['distance'] < self.__closeRadius:
+      print "cache within ", self.__closeRadius, "m"
+      return data[0] 
+  
+    # if we aren't moving return the first cache since bearing would
+    # be useless.
+    if speed < 5:
+      print "low speed.  using full search radius"
+      return data[0]
+
+    # sift through caches looking for closest one within our travel path
     for row in data:
       minBearing = (bearing - self.__bearingError) % 360
       maxBearing = (bearing + self.__bearingError) % 360
@@ -92,7 +109,6 @@ def main(db):
   lat_widget = sl.add_string_widget("lat","", y=1)
   lon_widget = sl.add_string_widget("lon","", y=2)
 
-  #title = s.add_title_widget("title","")
   title = s.add_scroller_widget("title",1,1,16,1,"h",1,"" )
   code = s.add_string_widget("code", "", y=2)
   dist = s.add_string_widget("dist", "", y=2, x=9)
@@ -100,37 +116,39 @@ def main(db):
 
   lat, lon = (0,0)
   bearing = 0
+  speed = 0
 
   while 1:
-    gpsinfo = gps_session.next()
-    if 'track' in gpsinfo.keys():
-      bearing = gpsinfo.track
-    if 'lat' in gpsinfo.keys() and 'lon' in gpsinfo.keys():
-      lat = gpsinfo.lat
-      lon = gpsinfo.lon
-      lat_widget.set_text("lat: %0.6f" % lat)
-      lon_widget.set_text("lon: %0.6f" % lon)
-    closest = db.findNearest(lat, lon, bearing)
+    # process all waiting GPS updates
+    while gps_session.waiting():
+      gpsinfo = gps_session.next()
+      if gpsinfo["class"] == 'TPV':
+        bearing = gpsinfo.track
+        lat = gpsinfo.lat
+        lon = gpsinfo.lon
+        speed = gpsinfo.speed
+        lat_widget.set_text("lat: %0.6f" % lat)
+        lon_widget.set_text("lon: %0.6f" % lon)
+        print gpsinfo['time'], bearing, lat, lon, speed
+
+    # find closest cache based on lat, lon, and bearing/speed
+    closest = db.findNearest(lat, lon, bearing, speed)
+
     if closest:
-      print closest
+      print closest['code'], closest['distance'], closest['bearing'], closest['lat'], closest['lon']
       s.set_priority("info")
       title.set_text(closest['description'].encode('ascii'))
       code.set_text(closest['code'].encode('ascii'))
       if closest['distance'] > 1000:
-        dist.set_text('%0.0fkm' % closest['distance']/1000.0)
+        dist.set_text('%0.0fkm' % (closest['distance']/1000.0))
       else:
         dist.set_text('%0.0fm' % closest['distance'])
-      if closest['distance'] < 200:
-        s.set_backlight("blink")
-      else:
-        s.set_backlight("on")
       bearing_widget.set_text(closest['human_bearing'])
     else:
       s.set_priority("hidden")
-    time.sleep(10)
 
 if __name__=='__main__':
-  db = CacheDatabase('db.sqlite', 3000, 500, 10)
+  db = CacheDatabase('db.sqlite', 3500, 100, 10)
   main(db)
 
     
